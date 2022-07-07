4ed55c26746b90a92f084097712802f2b696edec
LiShuxue <1149926505@qq.com>
2022-07-07 19:13:51 +0800

    feat: 更改文件类型

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from './axios';
 
 // 表单信息提交到后台
-export const submitFormInfo = (params) => {
+export const submitFormInfo = (params: any) => {
   return axios({
     method: 'post',
     url: 'https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth',
